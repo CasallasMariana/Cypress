94b562da37b05daca6f15631554e5eed44beed75
Mariana Casallas <casallasmariana912@gmail.com>
2024-08-14 12:46:16 -0500

    Se agrega la continuación de la prueba registrouser y se agregar la prueba de login

--- cypress/e2e/registrousuario.cy.js
+++ cypress/e2e/registrousuario.cy.js
@@ -4,13 +4,13 @@ describe('Validar que ingrese a la pantalla de registro usuario', () => {
       cy.get('.text-center > .font-bold').click();//Dar clic en sign Up
       cy.get('.text-4xl').should('be.visible');//validar el texto"Sign Up"
       cy.get(':nth-child(1) > .label > .label-text').should('be.visible');
-      cy.get('#full-name').type('Catañeda');
+      cy.get('#full-name').type('Martha Catañeda');
       cy.get(':nth-child(2) > .label > .label-text').should('be.visible');
-      cy.get('#email').type('Martha1222@hotmail.com');
+      cy.get('#email').type('Martha122@hotmail.com');
       cy.get(':nth-child(3) > .label > .label-text').should('be.visible');
-      cy.get('.join > #password').type('Martha123');
+      cy.get('.join > #password').type('Martha123+');
       cy.get(':nth-child(4) > .label > .label-text').should('be.visible');
-      cy.get('.join > #confirm-password').type('Martha123');
+      cy.get('.join > #confirm-password').type('Martha123+');
       cy.get(':nth-child(3) > .ng-untouched > .join > .btn').click();
       cy.get('.mb-4 > .join > .btn').click();
       cy.get(':nth-child(3) > .ng-untouched > .join > .btn').click();
@@ -21,9 +21,9 @@ describe('Validar que ingrese a la pantalla de registro usuario', () => {
       //prueba para el sign in
       cy.get('.text-4xl').should('be.visible');
       cy.get('.form-control.w-full > .label > .label-text').should('be.visible');
-      cy.get('#email').type('Martha1222@hotmail.com');
+      cy.get('#email').type('Martha122@hotmail.com');
       cy.get('.mb-4 > .label > .label-text').should('be.visible');
-      cy.get('.join > #password').type('Martha123');
+      cy.get('.join > #password').type('Martha123+');
       cy.get('.join > .btn').click();
       cy.get('.join > .btn').click();
       cy.get('[type="submit"]').click();
@@ -33,6 +33,13 @@ describe('Validar que ingrese a la pantalla de registro usuario', () => {
       cy.get('.max-w-lg').should('be.visible');
       cy.get('img').should('be.visible');
       cy.get('img').click();
-    
+    //Realizar el cierre del login
+    cy.contains('a', 'Logout').click();
+    //Validar que si está en la pantalla de Sign In.
+    cy.url().should('include', 'https://test-qa.inlaze.com/auth/sign-in');
+    cy.get('.text-4xl').should('be.visible');
+
+
+
     })
   })
