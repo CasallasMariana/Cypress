f7c825acb736d5b5f4df8eb794fc0b2e6f40d3e7
Mariana Casallas <casallasmariana912@gmail.com>
2024-09-05 18:14:34 -0500

    Se agregan archivos faltantes

---
+++ cypress/support/e2e.js
@@ -0,0 +1,4 @@
+// cypress/support/e2e.js (o cypress/support/index.js en versiones anteriores)
+
+// Importa el archivo de comandos para que los comandos personalizados estén disponibles
+import './commands';
