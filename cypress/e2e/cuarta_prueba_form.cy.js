5b441eef15bc3adc1b7f16afbb283e311f27851b
Mariana Casallas <casallasmariana912@gmail.com>
2024-09-23 16:41:40 -0500

    primera parte de la prueba con cy.get, cy.type y cy.clear

--- cypress/e2e/cuarta_prueba_form.cy.js
+++ cypress/e2e/cuarta_prueba_form.cy.js
@@ -1,8 +1,13 @@
-describe ("Ejercicio con aserciones",()=>{
+describe ("Aprender a interactuar y probar formularios en Cypress.",()=>{
     beforeEach(()=>{
-    cy.visit('https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_submit')
+    cy.visit('https://www.selenium.dev/selenium/web/web-form.html')
     })
-
-
-
+    it("Se realiza prueba con cy.get(), cy.type(), cy.select(), y cy.clear()",()=>{
+cy.get('h1').should('have.text',"Web form");
+cy.get('#my-text-id').type("Ingreso texto");
+cy.get(':nth-child(1) > :nth-child(2) > .form-control').type("MC");
+cy.get(':nth-child(1) > :nth-child(3) > .form-control').type("Prueba ingreso info en formulario");
+cy.get(':nth-child(1) > :nth-child(3) > .form-control').clear();
+cy.get(':nth-child(1) > :nth-child(3) > .form-control').type("Prueba ingreso segunda info en formulario");
+    })
 })
